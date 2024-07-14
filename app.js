4eb4a9ca0be6b03f392e79a18019c2a232095fcd
Abhilash <abhilash365366@gmail.com>
2024-07-15 01:46:05 +0530

    fourth check

---
+++ app.js
@@ -0,0 +1,106 @@
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
+let vidCon = document.querySelector(".vid-con");
+
+function curseranime(){
+    
+
+vidCon.addEventListener("mousemove",function(co){
+    gsap.to(".curser",{
+
+left:co.x ,
+top:co.y,
+
+    })
+})
+
+
+vidCon.addEventListener("mouseleave",function(co){
+    gsap.to(".curser",{
+opacity:0,
+
+scale:0,
+    })
+})
+
+
+vidCon.addEventListener("mouseenter",function(co){
+    gsap.to(".curser",{
+opacity:1,
+zIndex:100,
+scale:1,
+    })
+})
+}
+
+
+curseranime();
+
+function loading(){
+    gsap.from("#page1 h1",{
+   
+        opacity:0,
+        y:100,
+        stagger:0.3,
+      delay:0.7,
+        duration:0.5,
+    })
+    
+    gsap.from( vidCon,{
+        opacity:0,
+        y:100,
+        
+      
+        duration:0.8,
+    })
+    
+}
+
+loading();
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to(".dot",{
+        left:dets.x,
+        top:dets.y,
+    })
+   
+})
+
+let page3 = document.querySelector("#page3");
+page3.addEventListener("mouseleave",function(){
+    gsap.to(".dot",{
+        scale:1,
+        backgroundColor: "rgb(222, 222, 94,0.5)",
+       
+    })
+})
+page3.addEventListener("mouseenter",function(){
+    gsap.to(".dot",{
+        scale:0,
+        
+    })
+})
+let img=document.querySelectorAll(".child");
+
+img.forEach(function (pass) {
+    pass.addEventListener("mouseenter",function(){
+        gsap.to(".dot",{
+    scale:1.2,
+    backgroundColor: "rgb(222, 222, 94)",
+    border:"none",
+        })
+    })
+    
+   pass.addEventListener("mouseleave",function(){
+        gsap.to(".dot",{
+    scale:0,
+        })
+    })
+    
+})
+
